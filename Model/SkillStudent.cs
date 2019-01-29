d1e2a75d66a8e84d3746b67801c3841bb5d058a5
Ilham Habibie <habibiilham12@gmail.com>
2019-01-29 14:45:17 +0700

    push Model

---
+++ Model/SkillStudent.cs
@@ -0,0 +1,16 @@
+﻿using Microservice.DataAccess.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.DataAccess.Model
+{
+    public class SkillStudent : BaseModel
+    {
+        public string Name { get; set; }
+        public virtual Student Students { get; set; }
+        public virtual Skill Skills { get; set; }
+    }
+}
